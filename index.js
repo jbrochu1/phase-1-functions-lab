05470bcf03de90d04950c33d2e0b484c94538ec0
Jason Brochu <jbrochu1@gmail.com>
2022-09-20 09:28:45 -0600

    commit tree 673d28dbb53366d8a5c3b8fdf6fa065871a59797

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+}
+distanceFromHqInFeet();
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start-destination) * 264;
+}
+distanceTravelledInFeet();
+
+function calculatesFarePrice(start, destination) {
+    const feetTraveled = distanceTravelledInFeet(start, destination);
+    if (feetTraveled <= 400) {
+        return 0;}
+        else if (feetTraveled > 2500) {
+            return "cannot travel that far";
+        }
+        else if (feetTraveled > 400 && feetTraveled <= 2000) {
+            return (feetTraveled - 400) * 0.02;
+        }
+        else if (feetTraveled > 2000 && feetTraveled < 2500) {
+            return 25;
+        };
+}
